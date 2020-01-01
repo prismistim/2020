71c4d5783b02403c3329b7763c3f9b933c55b0a8
prismistim <prismistim@hotmail.com>
2020-01-01 14:40:57 +0900

    Update content

--- pages/fortune.vue
+++ pages/fortune.vue
@@ -3,20 +3,21 @@
     section
       h2.section-title Omikuji 2020
       p.description ※このおみくじは何度でも無料で引くことができます
-      .fortune-panel(v-if="result")
-        h3.sub-title 結果
-        .fortune-section
-          .fortune-title あなたの運勢は、たぶん
-          .fortune-result#result {{ result }}
-          .fortune-result-detail#detail {{ detail }}
-        h3.sub-title おすすめの音楽
-        .fortune-section
-          .fortune-music(v-if="result")
-            iframe(width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" v-bind:src="'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/' + trackId + '&color=%2364c6de&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true'")
-        h3.sub-title 結果をシェアする
-        .share-btn-group
-          a.twitter-share-button(v-bind:href="'https://twitter.com/intent/tweet?text=おみくじを引いたら「' + result + '」が出ました！あなたもくだらないおみくじにチャレンジ！ https://prismistim.github.io/2020'" data-size="large") ツイート
-          .line-it-button(data-lang="ja" data-type="share-a" data-ver="3" data-url="https://prismistim.github.io/2020/" data-color="default" data-size="large" data-count="false" style="display: none;")
+      transition(name="fade")
+        .fortune-panel(v-if="result")
+          h3.sub-title 結果
+          .fortune-section
+            .fortune-title あなたの運勢は、たぶん
+            .fortune-result#result {{ result }}
+            .fortune-result-detail#detail {{ detail }}
+          h3.sub-title おすすめの音楽
+          .fortune-section
+            .fortune-music(v-if="result")
+              iframe(width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" v-bind:src="'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/' + trackId + '&color=%2364c6de&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true'")
+          h3.sub-title 結果をシェアする
+          .share-btn-group
+            a.twitter-share-button(v-bind:href="'https://twitter.com/intent/tweet?text=おみくじを引いたら「' + result + '」が出ました！あなたもくだらないおみくじにチャレンジ！ https://prismistim.github.io/2020'" data-size="large") ツイート
+            .line-it-button(data-lang="ja" data-type="share-a" data-ver="3" data-url="https://prismistim.github.io/2020/" data-color="default" data-size="large" data-count="false" style="display: none;")
       button.try-btn(@click="loadFortune()") 引く
 
       script(src="https://d.line-scdn.net/r/web/social-plugin/js/thirdparty/loader.min.js" async="async" defer="defer")
@@ -41,6 +42,7 @@ export default {
   },
   methods: {
     loadFortune: function() {
+      window.scrollTo(0, 0);
       let rand = getRand(0, 7)
       this.result = fortune_data[rand].fortune
       this.detail = fortune_data[rand].detail
--- pages/index.vue
+++ pages/index.vue
@@ -18,25 +18,31 @@
       h2.section-title Omikuji 2020
       p.description 運勢とおすすめの音楽を勝手に押し付けます(くだらないので注意)
       nuxt-link.link-btn(to="/fortune") おみくじを引く
-    //- section
-    //-   h2.section-title 2019
-    //-   ul.done-list
-    //-     li.list-item
-    //-       h3.list-item-title 音楽を作った
-    //-     li.list-item
-    //-       h3.list-item-title 寄せ書き用サイトを作った
-    //-     li.list-item
-    //-       h3.list-item-title 広報を作った
-    //- section
-    //-   h2.section-title 2020
-    //-   ul.done-list
-    //-     li.list-item
-    //-       h3.list-item-title 音楽ゲームを完成させる
-    //-     li.list-item
-    //-       h3.list-item-title 寄せ書き用サイトを作った
-    //-     li.list-item
-    //-       h3.list-item-title 広報を作った
-
+    section
+      h2.section-title 2019
+      ul.done-list
+        li.list-item
+          h3.list-item-title 音楽を作った
+          p.description 2019年はBMS衆議院選とBOFXVという音楽ゲームのイベントに出ました。音楽もけっこう自分の作りたい音が作れるようになってきた
+            | のでこの調子で頑張りたいですね。
+          iframe(width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/635567031&color=%2364c6de&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true")
+          iframe(width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/685538011&color=%2364c6de&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true")
+        li.list-item
+          h3.list-item-title 寄せ書き用サイトを作った
+          p.description 卒業アルバムの寄せ書き部分が非常に狭かったので、アルバムにQRコードを載せてWebサイトに上げることにしました。
+            | 覚えたてのVueで作ったのですが、Googleフォームから投稿 -> スプレッドシートをVueで読み込みみたいな感じで、電気情報工学科らしい感じに
+            | なったのではないかと思います。
+        li.list-item
+          h3.list-item-title 広報を作った
+          p.description 某自治体の広報を縁あって作ることになりました。はじめてのDTP作業は本当に探りさぐりだったので、ド素人の出来ではあったものの
+            | いい経験になりました。
+    section
+      h2.section-title 2020
+      ul.done-list
+        li.list-item
+          h3.list-item-title 音楽ゲームを完成させる
+          p.description 半年以上前からUnityをベースにした音楽ゲーム制作をチームで行っています。今年の春にはなんとかリリースしたいなと思っていますので、
+            | 良かったら遊んでください。
 
 </template>
 
